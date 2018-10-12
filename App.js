2c7b6aca9469336d98ffa2313d748a52730df928
sajol <sajolkk@gmail.com>
2018-10-12 19:38:04 +0600

    Create reandomized greeting component

--- App.js
+++ App.js
@@ -10,8 +10,8 @@ export default class App extends Component {
             <View style={styles.container}>
                 <Text>Hello World!</Text>
                 <Banana/>
-                <Text>Rendering simple greeting list</Text>
-                <SimpleGreetingList/>
+                <Text>Rendering simple randomized greeting list</Text>
+                <SimpleRandomizedGreetingList names={['Sajol', 'John', 'Christ']}/>
             </View>
         );
     }
@@ -29,6 +29,55 @@ class SimpleGreetingList extends Component {
                 <Greeting name='Christ'/>
             </View>
         );
+    }
+}
+
+
+class SimpleRandomizedGreetingList extends Component {
+
+    constructor(props) {
+
+        super(props);
+
+        this.state = {
+            names: this.props.names
+        };
+
+        // Toggle the state every second
+        setInterval(() => {
+            this.setState(previousState => {
+                return {isShowingText: this.shuffleArray(previousState.names)};
+            });
+        }, 5000);
+    }
+
+    render() {
+
+        return (
+            <View style={{alignItems: 'center'}}>
+                {this.state.names.map((prop, key) => {
+                    return (
+                        <Greeting key={key} name={prop}/>
+                    );
+                })}
+            </View>
+        );
+    }
+
+    /**
+     * Randomize array element order in-place.
+     * Using Durstenfeld shuffle algorithm.
+     */
+    shuffleArray(array) {
+        if (array) {
+            for (let i = array.length - 1; i > 0; i--) {
+                const j = Math.floor(Math.random() * (i + 1));
+                const temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+        return array;
     }
 }
 
