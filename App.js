c3ac8a14561dbf639b42c07ef58dcf56cda6128b
sajol <sajolkk@gmail.com>
2018-10-12 18:51:39 +0600

    Create few components

--- App.js
+++ App.js
@@ -1,21 +1,43 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import Banana from "./component/Banana";
+import Greeting from "./component/Greeting";
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
+
+export default class App extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text>Hello World!</Text>
+                <Banana/>
+                <Text>Rendering simple greeting list</Text>
+                <SimpleGreetingList/>
+            </View>
+        );
+    }
 }
 
+
+class SimpleGreetingList extends Component {
+
+    render() {
+
+        return (
+            <View style={{alignItems: 'center'}}>
+                <Greeting name='Sajol'/>
+                <Greeting name='John'/>
+                <Greeting name='Christ'/>
+            </View>
+        );
+    }
+}
+
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
